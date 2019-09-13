eb21f389ec0306a68f21c3ca21911c31d34eb9fa
Jacob Stenson <jacob@nosnets.co.uk>
2019-09-14 00:46:41 +0100

    Polishing, fixing issues.

--- index.js
+++ index.js
@@ -103,38 +103,53 @@ bot.on('message', message=>{
 })
 
 bot.on('presenceUpdate', async (oldMember,newMember) => {
-    if(oldMember.presence.game !== newMember.presence.game){ // If the user's activity changes.
+    // If the game has changed...
+    if(oldMember.presence.game !== newMember.presence.game){
+        botsRole = newMember.guild.roles.find(x => x.name == 'bots')
+        hasBotsRole = newMember.roles.has(botsRole.id)
 
-        if(newMember.presence.game != null){
+        // If the presence is nothing, return. Or if the member has the bot role.
+        if((newMember.presence.game == null) || (hasBotsRole) || (newMember.presence.game == 'Spotify')){
+            return;
+        }
+        
+        var memberGameString = newMember.presence.game.toString();
+        console.log(newMember.displayName+" : "+ memberGameString)
+
+        // See if the application the user is running is whitelisted.
+        if(memberGameString in whiteListedApps){
+
             console.log("-- Someone's presence was updated. --")
-            memberGameString = newMember.presence.game.toString();
-
             console.log(newMember.displayName+": "+memberGameString);
     
-            // Do nothing if the user is using one of the blacklisted applications.
-            if(memberGameString in whiteListedApps){
-    
-                // Set roleName equal to the abreviated game name.
-                var roleName = whiteListedApps[memberGameString];
-    
-                var role = newMember.guild.roles.find(x => x.name == roleName);
-                // Search could not find the role by the specific game name (game name could be abreviated).
-                if(!role) {
-                    console.log("that role does not exist, creating it.")
-                    // Waits till the role is created, then carries on.
-                    await newMember.guild.createRole({name:roleName, mentionable:true})
-                    // Find the role by the name
-                    
-                }
-                var roleToAdd = newMember.guild.roles.find(x => x.name == roleName);
+            // Set roleName equal to the abreviated game name.
+            var roleName = whiteListedApps[memberGameString];
+
+            var role = newMember.guild.roles.find(x => x.name == roleName);
+            // Search could not find the role by the specific game name (game name could be abreviated).
+            if(!role) {
+                console.log("that role does not exist, creating it.")
+                // Waits till the role is created, then carries on.
+                await newMember.guild.createRole({name:roleName, mentionable:true})
+                // Find the role by the name
+                
+            }
+            var roleToAdd = newMember.guild.roles.find(x => x.name == roleName);
+
+            // Does the person have the role already?
+            hasRole = newMember.roles.has(roleToAdd.id)
+            if(!hasRole){
+                // If they dont have the role...
                 newMember.addRole(roleToAdd);
                 console.log("Gave "+newMember.displayName+" the "+roleToAdd.name+" role.")
             }
-            else{
-                console.log(newMember.displayName+"'s Application is not whitelisted. ("+memberGameString+").")
-            }
+                
+        }else{
+            console.log(newMember.displayName+"'s Application is not whitelisted. ("+memberGameString+").")
         }
-	}
+    
+    }
+    
 });
 
 
