d622178e3cf0e39535a2449485b8a384a8885d69
dylhack <dylhack@outlook.com>
2019-09-29 17:27:45 -0500

    Grouped variables

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,8 @@ bot.on('presenceUpdate', async (oldMember: Discord.GuildMember, newMember: Disco
     if (oldMember.presence.game !== newMember.presence.game) {
         // If the presence is nothing, return. Or if the member has the bot role.
         if ((newMember.presence.game == null)) return;
-
         let memberGameString: string = newMember.presence.game.toString();
-        console.log(`${newMember.displayName}: ${memberGameString}`);
+        console.log(`${newMember.displayName}: "${memberGameString}"`);
 
         // See if the application the user is running is whitelisted.
         if (whiteListedApps.has('memberGameString')) {
@@ -45,26 +44,22 @@ bot.on('presenceUpdate', async (oldMember: Discord.GuildMember, newMember: Disco
             // Set roleName equal to the abbreviated game name.
             let roleName: string | undefined = whiteListedApps.get(memberGameString);
             let role: Discord.Role | undefined = newMember.guild.roles.find((x: Discord.Role) => x.name == roleName);
+            let roleToAdd: Discord.Role | undefined = newMember.guild.roles.find(x => x.name == roleName);
+            let hasRole: boolean = newMember.roles.has(roleToAdd.id);
+
             // Search could not find the role by the specific game name (game name could be abbreviated).
             if (!role) {
                 console.log("that role does not exist, creating it.");
                 // Waits till the role is created, then carries on.
                 await newMember.guild.createRole({name: roleName, mentionable: true});
             }
-            let roleToAdd: Discord.Role | undefined = newMember.guild.roles.find(x => x.name == roleName);
-
             // Does the person have the role already?
-            let hasRole: boolean = newMember.roles.has(roleToAdd.id);
             if (!hasRole) {
                 // If they dont have the role...
                 await newMember.addRole(roleToAdd);
-                console.log(`Gave ${newMember.displayName} the ${roleToAdd.name} role.`)
-            } else {
-                console.log(`${newMember.displayName} already has the role.`)
-            }
-        } else {
-            console.log(`${newMember.displayName}'s Application is not whitelisted. (${memberGameString}).`)
-        }
+                console.log(`Gave ${newMember.displayName} the ${roleToAdd.name} role.`);
+            } else console.log(`${newMember.displayName} already has the role.`);
+        } else console.log(`${newMember.displayName}'s Application is not whitelisted. (${memberGameString}).`);
     }
 });
 
