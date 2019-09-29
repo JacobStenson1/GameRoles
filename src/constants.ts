418f173dae8e8ba7814c4861ed84dbd9734bc233
dylhack <dylhack@outlook.com>
2019-09-29 17:56:32 -0500

    Parsed token on runtime

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,9 @@ import {readFileSync} from "fs";
 
 export const dataLocation: string = resolve(`${__dirname}/../data/`);
 export const PREFIX: string = '/';
-export const TOKEN: string = readFileSync(`${dataLocation}/token.json`).toString();
+export const TOKEN: string = JSON.parse(
+    readFileSync(`${dataLocation}/token.json`).toString()
+);
 export const whiteListedApps: Map<string, string> = new Map(
     JSON.parse(
         readFileSync(`${dataLocation}/whitelist.json`).toString()
