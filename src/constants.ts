4ff4fc5a552fce5180e1a787d21c1ceb3ed9176e
dylhack <dylhack@outlook.com>
2019-09-30 10:20:20 -0500

    Added saving system

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import {resolve} from "path";
+import * as fs from "fs";
 import {readFileSync} from "fs";
 
 export const dataLocation: string = resolve(`${__dirname}/../data/`);
@@ -11,3 +12,14 @@ export const whiteListedApps: Map<string, string> = new Map(
         readFileSync(`${dataLocation}/whitelist.json`).toString()
     )
 );
+
+process.on('beforeExit', () => {
+    let exportArray: [string, string][] = [];
+    Array.from(whiteListedApps)
+        .forEach((keyValue: [string, string]) => {
+            let game: string = keyValue[0];
+            let abbreviation: string = keyValue[1];
+            exportArray.push([game, abbreviation]);
+        });
+    fs.writeFileSync(`${dataLocation}/whitelist.json`, JSON.stringify(exportArray));
+});
