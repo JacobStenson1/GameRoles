d9ea840962b0a067ed1b4d3b0aba0a092b7b7477
stensonj <stensonj@uni.coventry.ac.uk>
2019-10-02 22:18:55 +0100

    Added some features and improvements.
    Added three new commands to the bot: 'delete', 'undo' and 'games'.
    - /delete will delete a specified game from the whitelisted apps, this command must be used in the form '/delete [Game name] [Role name]'.
    - /undo will undo the previous action on the whitelisted apps, if a user has previously added something /undo will delete it, vice versa for re-adding something after it is deleted (although delete has not been implemented yet but will be in future).
    - /games prints out the key : value pairs for all the games in the whitelist in the form 'game : role'.
    
    Only users with the 'move' role can use the commands: 'add', 'delete', 'undo' and 'games'. This is added to stop users without permission using the commands, users will also be told they do not have sufficient permissions to use a command.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 
 // The minimum allowable time between saves (milliseconds)
 const minumumSaveInterval = 10000;
-// Whether or not the whitlist is saved
+// Whether or not the whitelist is saved
 var saved = true;
 
 var tokenFile = require('./TokenFile.json');
@@ -29,42 +29,122 @@ function save(){
     jsonfile.writeFile("./whitelist.json", whiteListedApps, { spaces: 2, EOL: '\r\n' }, function(err){
         if (err) throw(err);
     })
+    console.log("Whitelist file saved.")
     saved = true;
 }
 
 //Calls when the bot first comes online
 bot.on('ready',function(){
     console.log('Bot is online.');
+    
+    // Variable setup.
+    var previousAction = ''
+    var objectAdded = {}
+    
     bot.user.setActivity('with your server.', {type:'messing'})
 })
 
 bot.on('message', message=>{
     let args = message.content.substring(prefix.length).split(" ");
+    let moveRole = message.guild.roles.find(x => x.name == 'move')
 
     // 0 is base command -- !ping help -- (ping in this case)
     switch(args[0]){
         case 'ping':
             message.channel.send("pong!");
             break;
+
         case 'website':
-            message.channel.send("google.com")
+            message.channel.send("https://github.com/JacobStenson1/GameRoles")
             break;
+
         case 'add':
-            var roleToAdd = args.pop()
-            var gameName = args.slice(1,args.length).join(" ")
-            if ((!gameName) && (!roleToAdd)){
-                message.reply("Please enter the correct command (/add [gameName] [role name]")
+            var hasPerms = message.member.roles.has(moveRole.id)
+            if (hasPerms){
+                var roleToAdd = args.pop()
+                var gameNameAdd = args.slice(1,args.length).join(" ")
+                if ((!gameNameAdd) && (!roleToAdd)){
+                    message.reply("Please enter the correct command (/add [gameName] [role name]")
+                }else{
+                    // Apply changes
+                    whiteListedApps[gameNameAdd] = roleToAdd;
+                    //var gameJustAdded = gameName
+                    console.log("Added game: "+gameNameAdd+" with role name: "+roleToAdd)
+                    message.reply("Added game: "+gameNameAdd+" with role name: "+roleToAdd)
+                    objectAdded = {'Game' : gameNameAdd, 'Role' :roleToAdd}
+                    previousAction = 'add'
+                    // Save
+                    scheduleSave();
+                }
             }else{
-                // Apply changes
-                whiteListedApps[gameName] = roleToAdd;
-                console.log("Added game: "+gameName+" with role name: "+roleToAdd)
-                message.reply("Added game: "+gameName+" with role name: "+roleToAdd)
-                // Save
-                scheduleSave();
+                message.reply("You do not have sufficient permissions to perform that command.")
             }
             break;
+
+        case 'delete':
+            var hasPerms = message.member.roles.has(moveRole.id)
+            if (hasPerms){
+                var roleToDelete = args.pop()
+                var gameNameDelete = args.slice(1,args.length).join(" ")
+                delete whiteListedApps[gameNameDelete]
+                console.log("The "+gameNameDelete+" game and "+roleToDelete+" role, has been deleted from the whitelist.")
+                message.reply("The "+gameNameDelete+" game and "+roleToDelete+" role, has been deleted from the whitelist.")
+                // Save
+                scheduleSave();
+                previousAction = 'delete'
+            }else{
+                message.reply("You do not have sufficient permissions to perform that command.")
+            }
+            break;
+
+        case 'undo':
+            var hasPerms = message.member.roles.has(moveRole.id)
+            if (hasPerms){
+                // If the file is saved...
+                if (saved){
+                    if (previousAction == 'add'){
+                        // Delete the game that has just been added.
+                        delete whiteListedApps[objectAdded['Game']]
+                        message.reply(objectAdded['Game'] +": "+objectAdded['Role']+" - has been deleted.")
+                        scheduleSave()
+        
+                    }else if (previousAction == 'delete'){
+                        console.log("placeholder, deleted...")
+                        //roleToDelete
+                        //gameNameDelete
+                        message.reply("Sorry, undoing a delete is not implemented yet.")
+                    }else{
+                        console.log("Issue.")
+                        message.reply("Undo is not implemented yet.")
+                    }
+                }else{
+                    message.reply("Please wait for the whitelist file to be saved.")
+                }
+            }else{
+                message.reply("You do not have sufficient permissions to perform that command.")
+            }
+            break;
+            
+        case 'games':
+            var hasPerms = message.member.roles.has(moveRole.id)
+            if (hasPerms){
+                var stringToSend = "-- Printing the whitelist of games -- \n"
+                var whiteListedAppsLength = Object.keys(whiteListedApps).length
+
+                for (i = 0; i < whiteListedAppsLength; i++) {
+                    var key = Object.keys(whiteListedApps)[i]
+                    var value = whiteListedApps[key]
+                    var keyValueGames = (key +" : "+value)
+                    stringToSend += (keyValueGames + "\n")
+                }
+                message.channel.send(stringToSend)
+            }else{
+                message.reply("You do not have sufficient permissions to perform that command.")
+            }
+            break;
+
         case 'info':
-            message.channel.send("The author of this bot is Salazhar. See the github for this bot here: https://github.com/JacobStenson1/DiscordGameRoleBot")
+            message.channel.send("The author of this bot is Salazhar. See the GitHub for this bot here: https://github.com/JacobStenson1/GameRoles")
             break;
     }
 })
